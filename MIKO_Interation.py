72a24ec9ea27578aec91474fb360b10b13186f81
Vishal Bakshi <bakshiv193@gmail.com>
2022-12-06 17:54:44 +0530

    first commit

---
+++ MIKO_Interation.py
@@ -0,0 +1,41 @@
+import pyttsx3,os,openai
+import json
+import openai
+class Engine:
+    engine = pyttsx3.init('sapi5')
+    
+    def __init__(self):
+        pass
+    
+    def speak(self,audio):
+        self.engine.say(audio)
+        self.engine.runAndWait()
+  
+
+    def openai(self):
+        
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+
+        response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt= "what is gender?",
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+        )
+        data_raw = response.get('choices')
+        for i in data_raw:
+            data = i.get('text')
+            self.speak(data)
+              
+          
+
+if __name__ == '__main__':
+    run = Engine()
+    run.openai()
+    
+
+
+
