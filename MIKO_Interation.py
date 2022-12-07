963f81685acd1c3eefdcf2619ddb7165ff341aa7
Vishal Bakshi <bakshiv193@gmail.com>
2022-12-07 17:45:24 +0530

    API Working with speech recognition

--- MIKO_Interation.py
+++ MIKO_Interation.py
@@ -2,6 +2,8 @@ import pyttsx3,os,openai
 import json
 import openai
 import speech_recognition as sr
+import datetime
+import sys
 class Engine:
     engine = pyttsx3.init('sapi5')
     
@@ -11,36 +13,56 @@ class Engine:
     def speak(self,audio):
         self.engine.say(audio)
         self.engine.runAndWait()
-  
+        
+    def wishMe(self):
+        hour = int(datetime.datetime.now().hour)
+        if hour>=0 and hour<12:
+            self.speak("Good Morning!")
+
+        elif hour>=12 and hour<18:
+            self.speak("Good Afternoon!")   
+
+        else:
+            self.speak("Good Evening!")  
+
+        self.speak("I am Jarvis Sir. Please tell me how may I help you")
+        
     def takeCommand(self):
+        #It takes microphone input from the user and returns string output
         r = sr.Recognizer()
-        with sr.Microphone as source:
+        with sr.Microphone() as source:
             print("Listening...")
-            r.pause_threshold = 2
+            r.pause_threshold = 1
             audio = r.listen(source)
+
         try:
-            print("Recognizing")
-            self.query = r.recognize_google(audio,language='en-in')
-            print(f"user said: {self.query}\n")
+            print("Recognizing...")    
+            query = r.recognize_google(audio, language='en-in')
+            # print(f"User said: {query}\n")
             
-        except Exception as e:
-            print("say that again please")
-            return "None"
+            # openai(self.query)
+        except Exception as e:  
+            print("Say that again please...")
+            return None
+        self.openai(query)
+        
+        # return query
                 
-                  
+   
       
-    def openai(self):
+    def openai(self,Text):
+        print(f"User said: {Text}\n")
         openai.api_key = os.getenv("OPENAI_API_KEY")
         
         response = openai.Completion.create(
         model="text-davinci-003",
-        prompt= "what is gender?",
+        prompt= Text,  
         temperature=0.7,
         max_tokens=256,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
-        )
+            )
         data_raw = response.get('choices')
         for i in data_raw:
             data = i.get('text')
@@ -50,7 +72,14 @@ class Engine:
 
 if __name__ == '__main__':
     run = Engine()
-    run.takeCommand()
+    run.wishMe()
+    
+    while True:
+        try:
+            run.takeCommand()
+        except KeyboardInterrupt:
+            sys.exit()
+            
     
 
 
